b928a9457e00b68ae76c2c8c94918219479e3e31
kaynanfujiro <kaynanfelipe28@hotmail.com>
2024-08-20 22:35:13 -0300

    Update Sobre mim

--- src/pages/SobreMim.tsx
+++ src/pages/SobreMim.tsx
@@ -9,7 +9,7 @@ export const SobreMim = () => {
             <Box padding={5}>
                 <BlinkingBorderImage />
             </Box>
-            <Text fontSize='2xl' color='#FEFDFB' id="typed-output" fontFamily='Roboto'>Olá eu Sou Kaynan Desenvolvedor Web Iniciante</Text>
+            <Text fontSize='2xl' color='#FEFDFB' id="typed-output" fontFamily='Roboto'>Olá! Eu sou Kaynan Santos, Desenvolvedor Web em início de carreira.</Text>
         </Flex>
     );
 }
